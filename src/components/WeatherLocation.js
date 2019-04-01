a06daab69059ac598defd88a50559aae806873e8
Caro <nidiacmh@gmail.com>
2019-03-31 18:53:04 -0600

    Add components

---
+++ src/components/WeatherLocation.js
@@ -0,0 +1,7 @@
+import React from 'react';//Se importa el paquete de react
+
+const WeatherLocation = () => (//arrow function 
+  <div> WeatherLocation</div>
+); //Este codigo se ejecuta con la ayuda de babel, es el cuerpo del componente
+
+export default WeatherLocation;
