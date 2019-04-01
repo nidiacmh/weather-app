f6d3f1baf83c10a4f624c7154f8bdba05aad7a69
Caro <nidiacmh@gmail.com>
2019-03-31 19:16:08 -0600

    Agregar las plantillas de la temperatura e informacion extra

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import WeatherExtraInfo from './WeatherExtraInfo';
+import WeatherTemperature from './WeatherTemperature';
 
 const WeatherData = ( ) => (
-  <div> Datos del clima </div>
+  <div> Datos del clima
+    <WeatherTemperature/>
+    <WeatherExtraInfo/>
+  </div>
 );
 
 export default WeatherData;
---
+++ src/components/WeatherExtraInfo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WeatherExtraInfo = () => (
+  <div> Información extra sobre el clima</div>
+);
+
+export default WeatherExtraInfo;
